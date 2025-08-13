770b245521d5e1447df1d87511fd3cee9a382256
Abhijit Desktop <abhijitnumber1@gmail.com>
2025-08-14 01:28:15 +0530

    navbar created with logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
 import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
+
 function App() {
 	const isLg = useBreakpointValue({ base: false, lg: true });
 	return (
-		<>
-			<Grid
-				templateAreas={{
-					base: `"nav" "main"`,
-					lg: `"nav nav" "aside main"`,
-				}}
-			>
-				<GridItem area={"nav"}>
-					<NavBar />
-				</GridItem>
+		<Grid
+			templateAreas={{
+				base: `"nav" "main"`,
+				lg: `"nav nav" "aside main"`,
+			}}
+		>
+			<GridItem area={"nav"}>
+				<NavBar />
+			</GridItem>
 
-				{isLg ? (
-					<GridItem area="aside" bg="green">
-						Aside
-					</GridItem>
-				) : null}
-				<GridItem area={"main"} bg={"blue"}>
-					Main
+			{isLg ? (
+				<GridItem area="aside" bg="green">
+					Aside
 				</GridItem>
-			</Grid>
-		</>
+			) : null}
+			<GridItem area={"main"} bg={"blue"}>
+				Main
+			</GridItem>
+		</Grid>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,40 @@
-import { Provider } from "@/components/ui/provider";
+import {
+	ChakraProvider,
+	createSystem,
+	defaultConfig,
+	defineConfig,
+} from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+const config = defineConfig({
+	theme: {
+		breakpoints: {
+			sm: "320px",
+			md: "768px",
+			lg: "960px",
+			xl: "1200px",
+		},
+		semanticTokens: {
+			colors: {
+				danger: { value: "{colors.red}" },
+			},
+		},
+		keyframes: {
+			spin: {
+				from: { transform: "rotate(0deg)" },
+				to: { transform: "rotate(360deg)" },
+			},
+		},
+	},
+});
 
+const system = createSystem(defaultConfig, config);
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<Provider>
+		<ChakraProvider value={system}>
 			<App />
-		</Provider>
+		</ChakraProvider>
 	</StrictMode>
 );
