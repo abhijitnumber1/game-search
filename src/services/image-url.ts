0b3f1b314f1e75019411586075c37d278ae7ecbf
Abhijit Desktop <abhijitnumber1@gmail.com>
2025-08-24 12:02:12 +0530

    handle no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,6 @@
+import noImage from "../assets/placeholder-image.png";
 const getOptimizedImageUrl = (url: string) => {
-	if (!url) return "";
+	if (!url) return noImage;
 	const index = url.indexOf("media/");
 	return (
 		url.slice(0, index + "media/".length) +
